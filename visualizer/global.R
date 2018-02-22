706ec4d24cc677a30623e63f4f6925e300c98547
elaustin <elaustin@uw.edu>
2018-02-22 15:33:56 -0800

    Remove the rCharts library.

--- visualizer/global.R
+++ visualizer/global.R
@@ -5,7 +5,7 @@ library(pacman)
 
 p_load("shiny","shinythemes","foreign","data.table","leaflet","leaflet.extras",
        "zoo","ggplot2", "riem")
-p_load_gh('ramnathv/rCharts')
+
 
 source("data_read_functions.R")
 source("get_external_data.R")
